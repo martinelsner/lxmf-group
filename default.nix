4bc6794b8d81be6917f7f0c5c259c1cef86e6c8f
Martin Elsner <martin.elsner@gmail.com>
2026-04-08 13:19:40 +0000

    Fixed the Nix expression.

--- default.nix
+++ default.nix
@@ -4,6 +4,20 @@ let
   python = pkgs.python3;
   versionFile = builtins.readFile ./lxmf_group/_version.py;
   version = builtins.head (builtins.match ''.*__version__ = "([^"]+)".*'' versionFile);
+
+  lxmfy = python.pkgs.buildPythonPackage {
+    pname = "lxmfy";
+    version = "1.6.1";
+    src = pkgs.fetchgit {
+      url = "https://git.quad4.io/LXMFy/LXMFy.git";
+      rev = "36e2b5c9210703b9cfd7867ddacda991574c53dc";
+      hash = "sha256-jEjvogqXoKTH5UbPFfgZsk5WnOYvwPY89itDFeO4LZ8=";
+    };
+    format = "pyproject";
+    build-system = [ python.pkgs.poetry-core ];
+    dependencies = with python.pkgs; [ rns lxmf ];
+    pythonRelaxDeps = [ "rns" ];
+  };
 in
 python.pkgs.buildPythonApplication {
   pname = "lxmf-group";
@@ -16,8 +30,10 @@ python.pkgs.buildPythonApplication {
   dependencies = with python.pkgs; [
     rns
     lxmf
+    lxmfy
     platformdirs
     qrcode
+    msgpack
   ];
 
   meta = {
